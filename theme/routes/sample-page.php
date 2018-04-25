06cf2ca8340109773e0072b98ddfa28cd0e3ee11
Tricia Leach <tleach@gmail.com>
2018-04-24 17:45:55 -0700

    add sample page

--- theme/routes/sample-page.php
+++ theme/routes/sample-page.php
@@ -2,9 +2,9 @@
   $context = Timber::get_context();
   $post = new TimberPost();
   $context['page'] = $post;
-  
+
   Timber::render( array(
-    'views/pages/page-' . $post->post_name . '.twig',
+    'views/pages/sample-page.twig',
     'views/pages/page.twig'
   ), $context );
 ?>
